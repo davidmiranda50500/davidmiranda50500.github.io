5a17a569ffe920e05afc77d4eb00d6bdb9bbdd49
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2016-08-16 14:02:01 -0300

    primeiro

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function(){
+	var $root = $('html, body');
+	var $menuicon = $("#menu-icon");
+	$('#side-menu a').click(function() {
+	    var href = $.attr(this, 'href');
+	    $root.animate({
+	        scrollTop: href=="#"? 0 : $(href).offset().top
+	    }, 500, function () {
+	        window.location.hash = href;
+	    });
+	    $menuicon.click();
+	    return false;
+	});
+	$menuicon.click(function(){
+		$(this).toggleClass('open');
+	});
+
+	$('.js-slideshow').responsiveSlides({
+        auto: false,
+        speed: 500,
+        maxwidth: 960,
+        nav: true
+	});
+	var $next = $(".rslides_nav.next");
+	var $prev = $(".rslides_nav.prev");
+	// $('.js-slideshow').on('swipe', function(ev){
+	// 	console.log(ev);
+	// });
+
+	$next.text(">");
+	$prev.text("<");
+});
